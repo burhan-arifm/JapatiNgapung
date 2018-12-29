9b9eb3a2c18d7cf06db848a81443a6c74ab36928
Burhan Arif Muhammad <burhan.arifm@hotmail.com>
2018-12-29 13:29:11 +0700

    init

---
+++ Assets/Scripts/Manuk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manuk : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private new Rigidbody2D rigidbody;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Don't Allow
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody.velocity = Vector2.zero;
+                rigidbody.AddForce(new Vector2(0, upForce));
+                animator.SetTrigger("Flap");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigidbody.velocity = Vector2.zero;
+        isDead = true;
+        animator.SetTrigger("Die");
+        GameController.instance.BirdDied();
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.instance.gameOver)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+    }
+}
